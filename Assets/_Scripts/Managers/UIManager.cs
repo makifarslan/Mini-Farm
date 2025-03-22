8e22b96d10675f8522923f73e04d9a00689dbd93
Mehmet Akif ARSLAN <makifyos@gmail.com>
2025-03-22 20:58:56 +0300

    UI Scale Effect
    - Added scale effect to resource UIs when a resource value changes.

--- Assets/_Scripts/Managers/UIManager.cs
+++ Assets/_Scripts/Managers/UIManager.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UniRx;
 using TMPro;
 using Zenject;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
@@ -13,17 +14,39 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        // Subscribe to resource changes to update UI
-        resourceManager.resources.ObserveEveryValueChanged(dict => dict.ContainsKey(ResourceType.Wheat) ? dict[ResourceType.Wheat] : 0)
-            .Subscribe(value => wheatText.text = value.ToString())
+        // Subscribe to resource changes to update UI and trigger the scale effect
+        resourceManager.resources
+            .ObserveEveryValueChanged(dict => dict.ContainsKey(ResourceType.Wheat) ? dict[ResourceType.Wheat] : 0)
+            .Subscribe(value => {
+                wheatText.text = value.ToString();
+                TextScaleEffect(wheatText);
+            })
             .AddTo(this);
 
-        resourceManager.resources.ObserveEveryValueChanged(dict => dict.ContainsKey(ResourceType.Flour) ? dict[ResourceType.Flour] : 0)
-            .Subscribe(value => flourText.text = value.ToString())
+        resourceManager.resources
+            .ObserveEveryValueChanged(dict => dict.ContainsKey(ResourceType.Flour) ? dict[ResourceType.Flour] : 0)
+            .Subscribe(value => {
+                flourText.text = value.ToString();
+                TextScaleEffect(flourText);
+            })
             .AddTo(this);
 
-        resourceManager.resources.ObserveEveryValueChanged(dict => dict.ContainsKey(ResourceType.Bread) ? dict[ResourceType.Bread] : 0)
-            .Subscribe(value => breadText.text = value.ToString())
+        resourceManager.resources
+            .ObserveEveryValueChanged(dict => dict.ContainsKey(ResourceType.Bread) ? dict[ResourceType.Bread] : 0)
+            .Subscribe(value => {
+                breadText.text = value.ToString();
+                TextScaleEffect(breadText);
+            })
             .AddTo(this);
+    }
+
+    private void TextScaleEffect(TMP_Text text)
+    {
+        if (DOTween.IsTweening(text.transform.parent))
+            return;
+
+        Vector3 startScale = text.transform.parent.localScale;
+        text.transform.parent.DOScale(startScale * 1.2f, 0.2f)
+            .OnComplete(() => text.transform.parent.DOScale(startScale, 0.2f));
     }
 }
